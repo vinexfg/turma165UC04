6465271a19a120ef387f557a7a288095580652b2
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2025-11-26 21:18:42 -0300

    atividade

---
+++ Aula01/aula.js
@@ -0,0 +1,8 @@
+const nome = "joao"
+console.log("string ->", nome, typeof nome);
+
+const idade = 12
+const preco = 19.99
+
+console.log('number ->', idade, typeof idade)
+console.log('number ->', preco, typeof idade)
---
+++ Aula01/exemploPromptSync.js
@@ -0,0 +1,43 @@
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+
+
+// const nome = prompt("Digite seu nome: ");
+// console.log(`ola, ${nome} o BACK comecou!`);
+
+
+// const idade = Number(prompt('Digite sua idade: '));
+// console.log(`Sua idade ${idade + 10}`);
+
+// questao 01
+const number1 = prompt(' DIgitie seu numero')
+const number2  = prompt(' DIgitie seu numero')
+const soma = Number(number1 + number2)
+
+// Questao 02
+const number3 = Number(prompt(' DIgitie seu numero'))
+const number4  = prompt(' DIgitie seu numero')
+const soma2 = Number(number1 - number2)
+
+// Questao03
+const number5 = Number(prompt(' DIgitie seu numero'))
+const number6  = Number(prompt(' DIgitie seu numero'))
+const soma3 = Number(number1 * number2)
+
+// questao04
+
+const number7 = Number(prompt(' DIgitie seu numero'))
+const number8 = Number(prompt(' DIgitie seu numero'))
+const soma4 = Number(number1 / number2)
+
+//questoa05
+const number9 = Number(prompt(' DIgitie seu numero'))
+const number10 = Number(prompt(' DIgitie seu numero'))
+const soma5 = Number((number1 + number2) /2)
+
+
+//questoa06
+const number9 = Number(prompt(' DIgitie seu numero'))
+const number10 = Number(prompt(' DIgitie seu numero'))
+const soma5 = Number((number1 + number2) /2)
+
