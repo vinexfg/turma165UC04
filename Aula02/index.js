679f11dccc2c80c42421b02b2aa1122dca28a4ea
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2025-12-16 21:26:28 -0300

    a

---
+++ Aula02/index.js
@@ -0,0 +1,9 @@
+import Usuario from "./script.js";
+
+
+
+let usuario01 = new Usuario('vinicius',18, ' Masculino', "84998-55-6768", 'vinnhiodograu45@hotmail.com', '0009.665')
+
+usuario01.reproduzir()
+
+
---
+++ Aula02/script.js
@@ -0,0 +1,29 @@
+
+
+
+export default class Usuario {
+  constructor(nome, idade, genero, telefone, email, cpf, cep, rg) {
+    this.nome = nome;
+    this.idade = idade;
+    this.genero = genero;
+    this.telefone = telefone;
+    this.email = email;
+    this.cpf = cpf;
+    this.cep = cep;
+    this.rg = rg;
+  }
+
+  reproduzir(){
+    console.log(     `
+        ${this.nome }
+        ${this.idade }
+        ${this.genero }
+        ${this.telefone }
+        ${this.email }
+        ${this.cpf }
+        ${this.cep }
+        ${this.rg }        
+    `);
+
+  }
+}
